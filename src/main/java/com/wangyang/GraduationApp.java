8602186b19c29e306dfdebf1fe340cc0e23dabd4
mysola <1577969706@qq.com>
2018-05-09 12:55:40 +0800

    first commit

--- src/main/java/com/wangyang/GraduationApp.java
+++ src/main/java/com/wangyang/GraduationApp.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 @MapperScan({"com.wangyang.mapper","com.wangyang.docIndex"})
 public class GraduationApp {
